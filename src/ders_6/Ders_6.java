f63abcee6bd12970ffa2e60f814913a39065091a
Barış Adsız <123736772+BarisAdsiz@users.noreply.github.com>
2023-12-13 14:20:51 +0300

    Add files via upload

---
+++ src/ders_6/Ders_6.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ders_6;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Barış Adsız
+ */
+public class Ders_6 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter number: ");
+        double number = input.nextDouble();
+        System.out.println(number);
+    }
+    
+}
